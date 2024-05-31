9913c78ddeb1b48745c9e6fbd7da607442ac612a
barrosflavio <flavi011@outlook.com>
2024-05-31 16:18:34 -0300

    First commit

---
+++ opensuse.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Atualiza os repositórios
+sudo zypper refresh
+
+# Instala o x11vnc
+sudo zypper install -y x11vnc
+
+# Obtém o nome do usuário atual
+read -p "Digite seu nome de usuário: " USERNAME
+
+# Solicita a senha do VNC
+sudo x11vnc -storepasswd /home/$USERNAME/.vnc/passwd
+
+# Caminho do arquivo
+FILE_NAME="startvnc.sh"
+
+# Define o conteúdo do script
+cat << EOF > "$FILE_NAME"
+#!/bin/bash
+
+# Iniciar o servidor VNC (x11vnc)
+x11vnc -display :0 -noxrecord -noxfixes -noxdamage -forever -bg -rfbauth /home/$USERNAME/.vnc/passwd
+EOF
+
+# Cria o arquivo e escreve o conteúdo
+echo -e "$SCRIPT_CONTENT" > "$FILE_NAME"
+
+# Move o arquivo para /usr/local/bin
+sudo mv startvnc.sh /usr/local/bin/
+
+echo "Arquivo /usr/local/bin/startvnc.sh criado com sucesso!"
